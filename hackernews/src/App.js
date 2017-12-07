c164ba45b446972eac074b7df592222d72aa3b1b
Tash Postolovski <npostolovski@gmail.com>
2017-12-07 19:59:05 +1100

    use new ES6 syntax for map function

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -26,18 +26,16 @@ class App extends Component {
     const user = { firstname: "React", lastname: "Reacterson"}
     return (
       <div className="App">
-        {list.map(function(item) {
-            return (
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </div>
-            );
-        })}
+        {list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )}
       </div>
     );
   }
