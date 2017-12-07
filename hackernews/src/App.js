c5f2515eab9cf7ce01b0b5e787d227a80e9ef08d
Tash Postolovski <npostolovski@gmail.com>
2017-12-07 19:38:27 +1100

    Make some changes to the content using JSX

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    var helloWorld = 'Welcome to the road to learn React';
+    var user = { firstname: "React", lastname: "Reacterson"}
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{helloWorld}</h2>
+        <p>Nice to meet you, {user.firstname} {user.lastname}!</p>
       </div>
     );
   }
